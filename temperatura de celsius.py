532b447deee6e5947b62164bc028daf738e7f125
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-15 23:15:49 -0500

    temperatura a celcius

---
+++ temperatura de celsius.py
@@ -0,0 +1,3 @@
+temperatura = float(input("Ingrese temperatura Fahrenheit: "))
+celsius = (5/9) * (temperatura - 32)
+print(f"{temperatura} grados Fahrenheit son equivalentes a {celsius} grados Celsius.")
