1ea8cd91e6115836c272774da80405f654b4a2a0
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-15 23:08:36 -0500

    suma y multiplicacion

---
+++ Suma y multiplicacion.py
@@ -0,0 +1,8 @@
+#suma y multiplicacion
+num1=int(input("ingrese un numero 1: "))
+num2=int(input("ingrese un numero 2: "))
+suma=num1+num2
+print(f"el resultado de la suma es: {suma}")
+num3=int(input("ingrese un numero nuevo: "))
+multiplicacion= suma*num3
+print(f"el resultado de la multiplicacion es: {multiplicacion}")
