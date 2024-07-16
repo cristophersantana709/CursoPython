5dedda87691532911f01f8c529045bb8f531c5e4
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-16 00:50:59 -0500

    menor

---
+++ menor.py
@@ -0,0 +1,8 @@
+#Determinar el numero meno#
+num1 = int(input("Ingresar el primer numero: "))
+num2 = int(input("Ingresar el segundo numero: "))
+if num1 < num2:
+    menor = num1
+else:
+    menor = num2
+print(f"El menor es: {menor}")
