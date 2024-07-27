12506ca3aed6003e3ade017bc5f51126f8e161cf
cristopher santana <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2024-07-26 21:07:09 -0500

    Lista de compras y materias

---
+++ ListadeCompras.py
@@ -0,0 +1,3 @@
+lista_compras=["huevos","leche","pan"]
+for lista in lista_compras:
+    print(lista)
---
+++ Materia.py
@@ -0,0 +1,3 @@
+Lista_materias=["mate","lenguaje","quimica"]
+for materia in Lista_materias:
+    print(materia)
